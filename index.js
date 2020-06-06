d10c68462c681f6ac1211c02cd779f646fc6909f
dionysos <denisbakhoum@gmail.com>
2020-06-06 23:32:07 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log(`Hello my name
-Is John.`)
+let person = {
+  name: 'John',
+  age: 22,
+  profession: 'programmer'
+}
+console.log(`Hello my name is ${person.name}, I am ${person.age} years old and I am a ${person.profession}.`)
