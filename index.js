271a63257a244387d9db63080f31ba8d1fa58145
dionysos <denisbakhoum@gmail.com>
2020-06-07 23:52:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ let person = {
   profession: 'programmer'
 }
 console.log(`Hello my name is ${person.name}, I am ${person.age} years old and I am a ${person.profession}.`)
+
+
+// Star shape
+for (let i = 1; i < 10; i++) {
+  for (let j = 0; j < i; i++) {
+    console.log("#");
+  }
+  console.log("\n");
+}
