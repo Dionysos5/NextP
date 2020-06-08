777cc80035964c2a204a0559d1879847c4180546
dionysos <denisbakhoum@gmail.com>
2020-06-08 11:05:03 +0000

    Chapter 2, exercise 1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let person = {
-  name: 'John',
-  age: 22,
-  profession: 'programmer'
+//Eloquent Javascript exercises
+
+//Chapter 2 : Code structure
+//Exercise 1: Triangle shape
+let count = 7;
+//Triangle with for loop
+for (let hashes = ''; hashes.length <= count; hashes += '#') {
+  console.log(hashes);
 }
-console.log(`Hello my name is ${person.name}, I am ${person.age} years old and I am a ${person.profession}.`)
-
-
-// Star shape
-for (let i = 1; i < 10; i++) {
-  for (let j = 0; j < i; i++) {
-    console.log("#");
-  }
-  console.log("\n");
+let hashes = '';
+//Triangle with while loop
+while (hashes.length <= count) {
+  hashes += '#';
+  console.log(hashes);
 }
