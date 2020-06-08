111b6519f71a73fab119f873d60d88f5fc5fc687
dionysos <denisbakhoum@gmail.com>
2020-06-08 11:19:45 +0000

    Chapter 2, exercise 2

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ while (hashes.length <= count) {
   hashes += '#';
   console.log(hashes);
 }
+
+//Chapter 2 : Code structure
+//Exercise 2: FizzBuzz
+
+for (let number = 1; number <= 100; number++) {
+  if (number % 3 == 0 && number % 5 == 0)
+    console.log('FizzBuzz');
+  else if (number % 3 != 0 && number % 5 == 0)
+    console.log('Buzz');
+  else if (number % 3 == 0)
+    console.log('Fizz');
+  else
+    console.log(number);
+}
